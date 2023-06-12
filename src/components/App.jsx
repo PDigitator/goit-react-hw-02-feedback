c62105b6c4faf6ab43e3983ff0071cd490af5cca
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-06-12 03:02:22 +0300

    Add styles for Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import { Title, Wrapper } from 'components/App.styled';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Wrapper>
+        <Title>Hello World, this is my first styled component!</Title>
+      </Wrapper>
+    </div>
+  );
 };
